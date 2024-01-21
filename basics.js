cd311f67541357c3d6bb308355eafe14609c4195
luyvannda <philips.joe329@gmail.com>
2024-01-21 16:27:36 +0700

    my first typescript file

---
+++ basics.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.log("Good Bye");
+var roundNumber = Math.round(2.666666);
+console.log(roundNumber);
---
+++ basics.ts
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.log("Good Bye");
+const roundNumber = Math.round(2.666666);
+console.log(roundNumber);
