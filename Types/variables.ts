4048687594656730b412bf9f03df95cf9e7b0256
Luy Vannda <philips.joe329@gmail.com>
2024-02-01 10:54:13 +0700

    learn about type inference and type any

--- Types/variables.ts
+++ Types/variables.ts
@@ -12,3 +12,36 @@ gameOver = true;
 
 let nothing: null = null;
 let foo: undefined = undefined;
+
+// Types Inference
+
+let tvShow = "The last of us";
+tvShow = "the second one";
+tvShow = 1;
+
+let isFull = true;
+isFull = false;
+isFull = "full";
+
+// Types: any is an escape hatch from typescript check and should be use sparingly.
+
+let bigApple: any = "Big Apple";
+bigApple = 1;
+bigApple = false;
+bigApple();
+bigApple.indexOf();
+
+// The only time when 'any' should be used it when we delayed declaring a variable's type because we don't know it type yet.
+
+const movies = ["Batman Begin", "Titanic", "FastX", "Mission Impossible"];
+
+let foundMovies: string;
+
+for (let movie of movies) {
+  if (movie === "Titanic") {
+    foundMovies = "Titanic";
+  }
+}
+
+foundMovies = 1;
+foundMovies = true;
