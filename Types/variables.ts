7034ec4fd99f37cc1bca581ab4bcd08b1cc5175d
Luy Vannda <philips.joe329@gmail.com>
2024-02-01 10:22:13 +0700

    learn to use typescript to declare primitive types

---
+++ Types/variables.ts
@@ -0,0 +1,14 @@
+let movieTitle: string = "Amadeus";
+movieTitle = "The monkey king";
+// movieTitle = 8;
+
+let numCatLives: number = 9;
+numCatLives += 1;
+// numCatLives = "nine";
+
+let gameOver: boolean = false;
+gameOver = true;
+// gameOver = "true";
+
+let nothing: null = null;
+let foo: undefined = undefined;
