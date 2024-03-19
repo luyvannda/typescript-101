088a4e8dcfa03482447628669d9a1a5b3b957184
luyvannda <luyvannda@gmail.com>
2024-03-19 16:14:04 +0700

    learned alternatives way to declare array type

--- Types/array.ts
+++ Types/array.ts
@@ -6,13 +6,20 @@ const ageList: number[] = [35, 33];
 ageList[0] = 36;
 console.log(ageList);
 
-const trueFalseAnswers: boolean[] = [
-  true,
-  false,
-  false,
-  true,
-  true,
-  false,
-  false,
-  true,
-];
+const trueFalseAnswers: boolean[] = [true, false, false];
+
+// alternative ways to declare a an array type
+const nameArr: Array<string> = ["George", "Mina"];
+const numArr: Array<number> = [1, 2, 3];
+const bools: Array<boolean> = [true, false];
+
+// example of declaring object type to use inside an array of object
+type Point = {
+  x: number;
+  y: number;
+};
+
+const coords: Point[] = [];
+coords.push({ x: 23, y: 8 });
+
+console.log(coords);
