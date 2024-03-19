59813d38270c5ca52f82de3f8386bc8b79ef51d0
luyvannda <luyvannda@gmail.com>
2024-03-19 16:01:39 +0700

    practice array type

---
+++ Types/array.ts
@@ -0,0 +1,18 @@
+const usersList: string[] = ["Joe", "Raksmey"];
+usersList.push("Mike");
+console.log(usersList);
+
+const ageList: number[] = [35, 33];
+ageList[0] = 36;
+console.log(ageList);
+
+const trueFalseAnswers: boolean[] = [
+  true,
+  false,
+  false,
+  true,
+  true,
+  false,
+  false,
+  true,
+];
