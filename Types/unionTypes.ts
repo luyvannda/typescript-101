3ac93f354fe3a9b69a5a70f39e5a7ec88f86e1de
luyvannda <luyvannda@gmail.com>
2024-03-27 10:48:54 +0700

    learn to use type narrowing with union types

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -17,3 +17,17 @@ type Loc = {
 
 let coordinates: Point | Loc = { x: 12, y: 24 };
 coordinates = { lat: 245.712, long: 14.085 };
+
+function printAge(age: number | string): void {
+  console.log(`You are ${age} years old`);
+}
+
+function calculateTax(price: number | string, tax: number): number {
+  if (typeof price === "string") {
+    price = parseFloat(price.replace("$", ""));
+  }
+  const result = price * tax;
+
+  return Number(result.toFixed(4)); // we use toFixed to limit the result to only 4 decimal places, however it will change result type to string, hence the need to convert it back to number.
+}
+console.log(calculateTax(45, 0.7));
